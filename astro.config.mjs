d04447cff00198e5e99ae2c651294b56b4533abd
kumokumo1653 <fjsda526hdbs@gmail.com>
2025-11-12 22:33:06 +0900

    静的リソース修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kumokumo1653.github.io',
+  base: '/Portfolio',
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()]
   }
